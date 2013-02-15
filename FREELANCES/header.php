243f1c868f1bccde0e2013c11c4fe6c616ef8b81
janchiqui <janchiqui@gmail.com>
2013-02-15 16:16:24 -0600

    este es el cuerpo de la pagina freelances

---
+++ FREELANCES/header.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<title>Freelances in Nicaragua</title>
+
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<link rel="stylesheet" type="text/css" href="css/body.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+
+</head>
+<body>
+
+<div id="contenedor">
+<div id="cabecera"> 
+	
+
+</div>
+
+<div id="barramenu">
+
+<div id="nav">
+	
+
+	<ul>
+		
+<li> <a title="Option 1" href="#"> Inicio </a> | </li>
+<li> <a title="Option 2" href="#"> Servicios</a> | </li>
+<li> <a title="Option 3" href="#"> Portafolio</a> |  </li>
+<li> <a title="Option 4" href="#"> Acerca de</a>  | </li>
+<li> <a title="Option 5" href="#"> Contactanos</a> |  </li>
+
+</ul>
+
+</div>
+</div>
+
+</div>
+
+</body>
+</html>
+
+
+
---
+++ FREELANCES/piepagina.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+
+	<div class="clearfooter"></div>
+	<div id="Footer">
+<img src="images/fon9.jpg"  border="1" style="height: 51px;
+ width: 1000px;" margin-top="200px">
+
+<b>
+	SmartSoft
+</b>
+<br>
+janchiqui@gmail.com
+<br>
+fjeibi@gmail.com
+
+
+
+</div>
