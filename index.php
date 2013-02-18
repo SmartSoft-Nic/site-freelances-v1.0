2634ed4177d4ddf20b0005e70c05881a0392203e
SmartSoft-Nic <freelances.nic@gmail.com>
2013-02-18 12:23:21 -0600

    Nuevo Diseno

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<?php
-//Incluimos la Conexion
-	include 'sqlUtil/conexion.php';
-//Abrimos la conexion
-$objectConnection = managedConnection::openConnection();
-//Cerramos la conexion
-managedConnection::closeConnection($objectConnection);
+
+<?
+//Probando la conexion.
+include ""
 ?>
